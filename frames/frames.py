5220176554032668095e9eb2e984522c696040b0
David Wood <davidwood@Davids-MacBook-Pro.local>
2019-08-06 16:10:29 -0400

    removed cv2 added imageio

---
+++ frames/frames.py
@@ -0,0 +1,40 @@
+# Program To Read video and Extract Frames
+import re
+import os
+import imageio
+
+# Function to extract frames
+def FrameCapture(path):
+
+    # Path to video file
+    #vidObj = cv2.VideoCapture(path)
+    # Used as counter variable
+    count = 0
+    # checks whether frames were extracted
+    success = 1
+
+    vid = imageio.get_reader(path,  'ffmpeg')
+
+    tail = os.path.split(path)[1]
+
+    for image in vid.iter_data():
+        imageio.imwrite(os.path.join('/pfs/out', os.path.splitext(tail)[0]+'%d.jpeg' % count), image)
+        count += 1
+
+    #while success:
+        # vidObj object calls read
+        # function extract frames
+        #success, image = vidObj.read()
+        # filename
+        #tail = os.path.split(path)[1]
+        # Saves the frames with frame-count
+        #cv2.imwrite(os.path.join('/pfs/out', os.path.splitext(tail)[0]+'%d.jpeg' % count), image)
+    #    count += 1
+
+# Driver Code
+if __name__ == '__main__':
+    # walk /pfs/images and call make_edges on every file found
+    for dirpath, dirs, files in os.walk('/pfs/videos'):
+        for file in files:
+            # Calling the function
+            FrameCapture(os.path.join(dirpath, file))
