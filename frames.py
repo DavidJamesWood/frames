ea18febd95f71703a2fb3c74a5741a619011ddd0
David Wood <davidwood@Davids-MacBook-Pro.local>
2019-08-07 18:30:58 -0400

    with updated docker image

--- frames.py
+++ frames.py
@@ -6,34 +6,23 @@ import imageio_ffmpeg as imageio
 # Function to extract frames
 def FrameCapture(path):
 
-    # Path to video file
-    #vidObj = cv2.VideoCapture(path)
     # Used as counter variable
     count = 0
     # checks whether frames were extracted
     success = 1
-
+    # Process video
     vid = imageio.get_reader(path, 'ffmpeg')
-
+    # file name
     tail = os.path.split(path)[1]
-
+    # iterate through frames
     for image in vid.iter_data():
+        # write each frame
         imageio.imwrite(os.path.join('/pfs/out', os.path.splitext(tail)[0]+'%d.jpeg' % count), image)
         count += 1
 
-    #while success:
-        # vidObj object calls read
-        # function extract frames
-        #success, image = vidObj.read()
-        # filename
-        #tail = os.path.split(path)[1]
-        # Saves the frames with frame-count
-        #cv2.imwrite(os.path.join('/pfs/out', os.path.splitext(tail)[0]+'%d.jpeg' % count), image)
-    #    count += 1
-
 # Driver Code
 if __name__ == '__main__':
-    # walk /pfs/images and call make_edges on every file found
+    # walk /pfs/videos and call frames on every file found
     for dirpath, dirs, files in os.walk('/pfs/videos'):
         for file in files:
             # Calling the function
